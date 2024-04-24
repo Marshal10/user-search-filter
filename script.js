0d3fde6488a5e92a9b4915287788b26e3252722f
Marshal10 <marshalmaleane123@gmail.com>
2024-04-24 21:29:52 +0530

    Display users

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ const searchInput=document.getElementById('filter')
 
 const listItems=[]
 
+function updateDom(users){
+    result.innerHTML=''
+    users.forEach((user)=>{
+        const listItem=document.createElement('li')
+        listItem.innerHTML=`
+        <img src="${user.picture.large}" alt="${user.name.first}">
+        <div class="user-info">
+            <h4>${user.name.first} ${user.name.last}</h4>
+            <p>${user.location.city}, ${user.location.country}</p>
+        </div>
+        `
+        listItems.push(listItem)
+        result.appendChild(listItem)
+    })
+}
+
 async function getUsers(){
     const res=await fetch("https://randomuser.me/api?results=50")
     const {results}=await res.json()
-    console.log(results)
+
+    updateDom(results)
 }
 
 getUsers()
